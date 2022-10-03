64fda233ad8c843f18176691935aa36aa1d0c24e
Nikita Bondarenko <110502150+TetSigma@users.noreply.github.com>
2022-10-03 15:27:07 -0700

    Appolo url change

--- my-project/apollo-client.js
+++ my-project/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "http://localhost:5001/api/unrealistic-camel",
+    uri: "https://reddit-lac.vercel.app/api/unrealistic-camel",
     headers:{
         Authorization: `airmont::stepzen.net+1000::11b95a2c574833389e4eac4c8b020d02b08ca959b698e142d0e93152dbfaaf49`
     },
